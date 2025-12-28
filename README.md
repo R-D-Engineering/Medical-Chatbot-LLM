0a258891b5aea24aad43130dce9e94c68ebd6488
Anoop P Hegde <anoop.techstorm@gmail.com>
2025-12-28 10:31:36 +0530

    added requirements.txt

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 open anaconda prompt console
+
 cd Medical-Chatbot-LLM
-conda create -n medibot python=3.12 -y
+
+conda create -n medibot python=3.10 -y
+
+conda deactivate 
+
 conda activate medibot
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages, setup
+
+setup(
+    name="medical_chatbot_llm",
+    version="0.1.0",
+    author="Anoop P Hegde",
+    author_email="anoop.techstorm@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
